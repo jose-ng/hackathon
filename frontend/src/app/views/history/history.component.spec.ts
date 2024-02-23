83cd4eb521e0cfa43c70d2ac0638d1f70842a385
Josiel Hernandez <josiel.hernandez.dev@gmail.com>
2024-02-23 15:45:07 -0600

    [add] dashboard  service

--- frontend/src/app/views/history/history.component.spec.ts
+++ frontend/src/app/views/history/history.component.spec.ts
@@ -1,19 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { historyComponent } from './history.component';
+import { By } from '@angular/platform-browser';
 
 describe('historyComponent', () => {
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [
-                historyComponent
-            ],
-        }).compileComponents();
-    });
+  let component: historyComponent;
+  let fixture: ComponentFixture<historyComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [historyComponent],
+    }).compileComponents();
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(historyComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
-    });
+    fixture = TestBed.createComponent(historyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(historyComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should have a title', () => {
+    const title = 'Historial';
+    const titleElement = fixture.debugElement.query(
+      By.css('.history__component__header')
+    ).nativeElement.textContent;
+    expect(titleElement).toContain(title);
+  });
 });
