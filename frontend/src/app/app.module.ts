15841256b368b351bbc615694f4e14117cb23e30
Alan Ibarra <alanibarralop@gmail.com>
2024-02-23 13:11:41 -0700

    Feat: history view mock

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { historyComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    historyComponent
   ],
   imports: [
     BrowserModule
